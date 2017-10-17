4da3d3ed1bf6d00dcf3b5120725a2fa702246963
Nabreas <andrejs.riz@googlemail.com>
2017-10-17 23:02:21 +0100

    initial commit

---
+++ app/src/main/java/pb/co/uk/hockeystats/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package pb.co.uk.hockeystats.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import pb.co.uk.hockeystats.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
